99e006d4b71c34e958758372f838ac0e56430cfb
jahtz <jahtz.dev@proton.me>
2025-04-19 21:21:35 +0200

    match version of cli and pyproject

--- src/cli/sbbbin_cli.py
+++ src/cli/sbbbin_cli.py
@@ -25,7 +25,7 @@ from rich.progress import (Progress, TextColumn, BarColumn, MofNCompleteColumn, 
 from sbbbin import SbbBinarizer
 
 
-__version__ = "0.0.1"
+__version__ = "0.2.0"
 __prog__ = "template"
 __footer__ = "Developed at Centre for Philology and Digitality (ZPD), University of Würzburg"
 
